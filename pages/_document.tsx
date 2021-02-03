87cfdf82ea1ea3abc6837bd9223f75c5a790230c
Michael Schultz <michaelwschultz@gmail.com>
2021-02-02 19:04:23 -0800

    Adds babel-plugin-styled-components to try and fix issue (#10)
    * adds babel-plugin-styled-components to try and fix issue
    * Removes custom _document
    * Adds _document back with geist helper
    * bumps version
    * Removes private tag from package.json
    * fixes typo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,30 +1,33 @@
-import Document from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from '@geist-ui/react'
 
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const initialProps = await Document.getInitialProps(ctx)
+    const styles = CssBaseline.flush()
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {styles}
+        </>
+      ),
     }
   }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
