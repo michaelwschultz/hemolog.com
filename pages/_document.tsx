504528ba6abeb882bee4a77c85433e7c5601c878
Michael Schultz <michaelwschultz@gmail.com>
2021-02-03 12:07:13 -0800

    Fixes fouc by rendering both styled-components and geist on the sever

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { CssBaseline } from '@geist-ui/react'
+import Document from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
+import { CssBaseline } from '@geist-ui/react'
 
-class MyDocument extends Document {
+export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const styles = CssBaseline.flush()
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         })
-
       const initialProps = await Document.getInitialProps(ctx)
+      const styles = CssBaseline.flush()
+
       return {
         ...initialProps,
         styles: (
@@ -30,18 +29,4 @@ class MyDocument extends Document {
       sheet.seal()
     }
   }
-
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
 }
-
-export default MyDocument
