1803462ea1fc0b18369539ec962311181aa73381
Michael Schultz <michaelwschultz@gmail.com>
2021-05-23 01:47:24 -0700

    Adds annual chart for visualizing infusion data by month (#25)
    * wip
    
    * Adds annual chart, fixes update button, and improves mobile view

--- components/homePage.tsx
+++ components/homePage.tsx
@@ -6,65 +6,76 @@ import InfusionTable from 'components/infusionTable'
 import Stats from 'components/stats'
 import SettingsForm from 'components/settingsForm'
 import EmergencyCard from 'components/emergencyCard'
-import { useAuth } from 'lib/auth'
-import useDbUser from 'lib/hooks/useDbUser'
+import Chart from 'components/chart'
 
 const HomePage = (): JSX.Element => {
   const smallerThanSmall = useMediaQuery('xs', { match: 'down' })
-  const { user } = useAuth()
-  const { person } = useDbUser(user?.uid || '')
 
-  const hasAllInfo = () => {
-    if (person) {
-      if (
-        person.factor &&
-        person.hemophiliaType &&
-        person.medication &&
-        person.severity
-      ) {
-        return true
-      }
-      return false
-    }
-    return false
-  }
+  // TODO(michaelwschultz): enable once Firebase caching is removed
+  // const { user } = useAuth()
+  // const { person } = useDbUser(user?.uid || '')
 
-  const welcomeHero = () => {
-    return (
-      <StyledHero>
-        <Text h4>Getting started with Hemolog</Text>
-        <Text h6 small>
-          Fill in how you're affected to get the most accurate stats and a
-          custom alert card. You can update these at anytime using the profile
-          tab. If you're medication or type of hemophilia doesn't show up in the
-          list, feel free to write it in.
-        </Text>
-        <Spacer />
-        <Grid.Container gap={2}>
-          <Grid xs={24} sm={12}>
-            <SettingsForm />
-          </Grid>
-          <Grid xs={24} sm={12}>
-            <StyledCenterCard>
-              <EmergencyCard forPrint />
-            </StyledCenterCard>
-          </Grid>
-        </Grid.Container>
-      </StyledHero>
-    )
-  }
+  // const [showWelcome, setShowWelcome] = useState(false)
+
+  // useEffect(() => {
+  //   if (person) {
+  //     if (
+  //       person.factor &&
+  //       person.hemophiliaType &&
+  //       person.medication &&
+  //       person.severity
+  //     ) {
+  //       return setShowWelcome(false)
+  //     }
+  //     return setShowWelcome(true)
+  //   }
+  // }, [person])
+
+  // const welcomeHero = () => {
+  //   return (
+  //     <StyledHero>
+  //       <Text h4>Getting started with Hemolog</Text>
+  //       <Text h6 small>
+  //         Fill in how you're affected to get the most accurate stats and a
+  //         custom alert card. You can update these at anytime using the profile
+  //         tab. If you're medication or type of hemophilia doesn't show up in the
+  //         list, feel free to write it in.
+  //       </Text>
+  //       <Spacer />
+  //       <Grid.Container gap={2}>
+  //         <Grid xs={24} sm={12}>
+  //           <SettingsForm />
+  //         </Grid>
+  //         <Grid xs={24} sm={12}>
+  //           <StyledCenterCard>
+  //             <EmergencyCard forPrint />
+  //           </StyledCenterCard>
+  //         </Grid>
+  //       </Grid.Container>
+  //     </StyledHero>
+  //   )
+  // }
 
   return (
     <>
-      {!hasAllInfo && (
+      {/* TODO(michaelwschultz): enable once firebase caching is removed */}
+      {/* {showWelcome && (
         <>
           {welcomeHero()}
           <Spacer y={3} />
         </>
-      )}
+      )} */}
 
       <Text h4>Insights</Text>
       <Stats />
+      <Spacer y={3} />
+
+      <Text h4>Annual overview ({new Date().getFullYear()})</Text>
+      <Text h6 type='secondary'>
+        Infusions are stacked by type (bleed, preventative, or prophy)
+      </Text>
+      <Chart />
+
       <Spacer y={3} />
       <Row justify='space-between' align='middle'>
         <Text h4>Infusions</Text>
