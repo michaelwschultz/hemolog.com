b195a31dfee6f32c58a984928a8fc53637b73f91
Michael Schultz <michaelwschultz@gmail.com>
2022-06-01 21:00:39 -0700

    remove setupNodeEvents

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
-    },
+    // setupNodeEvents(on, config) {
+    //   // implement node event listeners here
+    // },
   },
 })
