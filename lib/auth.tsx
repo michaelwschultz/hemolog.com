6382f1792ff8d8ca18d2cf48f86e244651788dda
Michael Schultz <michaelwschultz@gmail.com>
2022-06-14 01:34:03 -0700

    change flag in auth

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -95,7 +95,7 @@ function useProvideAuth() {
 
   const auth = firebase.auth()
   // use emulator if developing locally
-  if (process.env.NEXT_PUBLIC_TEST_AGAINST_PROD !== 'true') {
+  if (process.env.NEXT_PUBLIC_USE_EMULATORS === 'true') {
     auth.useEmulator('http://localhost:9099')
   }
 
