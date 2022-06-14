cd01b282f3b2087a12f397b0838b32d01a52fdd5
Michael Schultz <michaelwschultz@gmail.com>
2022-06-14 00:58:20 -0700

    removes cypress downloads folder, adds retries

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,15 +5,11 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default defineConfig({
+  retries: 3,
   e2e: {
     baseUrl: 'http://localhost:3000',
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
     // },
-  },
-  env: {
-    googleRefreshToken: process.env.GOOGLE_REFRESH_TOKEN,
-    googleClientId: process.env.REACT_APP_GOOGLE_CLIENTID,
-    googleClientSecret: process.env.REACT_APP_GOOGLE_CLIENT_SECRET,
   },
 })
