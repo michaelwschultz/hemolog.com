0e75e21eee9b94f31dc71e1884b50ec9d6b7e0a7
Michael Schultz <michaelwschultz@gmail.com>
2025-09-21 23:41:42 -0700

    lint changes

--- lib/auth.tsx
+++ lib/auth.tsx
@@ -1,226 +1,226 @@
-import { useState, useEffect, useContext, createContext } from "react";
-import cookie from "js-cookie";
+import { useState, useEffect, useContext, createContext } from 'react'
+import cookie from 'js-cookie'
 
-import firebase, { firestore } from "lib/firebase";
-import { createUser } from "lib/db/users";
-import { generateUniqueString } from "lib/helpers";
-import LoadingScreen from "components/loadingScreen";
-import type { UserType } from "lib/types/users";
-import Router from "next/router";
+import firebase, { firestore } from 'lib/firebase'
+import { createUser } from 'lib/db/users'
+import { generateUniqueString } from 'lib/helpers'
+import LoadingScreen from 'components/loadingScreen'
+import type { UserType } from 'lib/types/users'
+import Router from 'next/router'
 
 type ContextProps = {
-	user: UserType | null;
-	loading?: boolean;
-	signout: any;
-	signinWithGoogle: any;
-	signinWithTestUser: any;
-};
-
-const authContext = createContext<Partial<ContextProps>>({});
-
-export function AuthProvider({
-	children,
-}: {
-	children: React.ReactNode;
-}): React.ReactElement {
-	const auth = useProvideAuth();
-	if (typeof window === "undefined") {
-		return <>{children}</>;
-	}
-	return <authContext.Provider value={auth}>{children}</authContext.Provider>;
+  user: UserType | null
+  loading?: boolean
+  signout: any
+  signinWithGoogle: any
+  signinWithTestUser: any
 }
 
-export const useAuth = () => useContext(authContext);
+const authContext = createContext<Partial<ContextProps>>({})
+
+export function AuthProvider({
+  children,
+}: {
+  children: React.ReactNode
+}): React.ReactElement {
+  const auth = useProvideAuth()
+  if (typeof window === 'undefined') {
+    return <>{children}</>
+  }
+  return <authContext.Provider value={auth}>{children}</authContext.Provider>
+}
+
+export const useAuth = () => useContext(authContext)
 
 function useProvideAuth() {
-	const [user, setUser] = useState<UserType | null>(null);
-	const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState<UserType | null>(null)
+  const [loading, setLoading] = useState(true)
 
-	const handleUser = async (rawUser: any) => {
-		if (rawUser) {
-			const user = await formatUser(rawUser);
-			const dbUser = await firestore.collection("users").doc(user.uid).get();
+  const handleUser = async (rawUser: any) => {
+    if (rawUser) {
+      const user = await formatUser(rawUser)
+      const dbUser = await firestore.collection('users').doc(user.uid).get()
 
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			const { token, ...userWithoutToken } = user;
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { token, ...userWithoutToken } = user
 
-			// TODO(michael) If user already exist then remove alertId
-			// so it isn't overwritten when the user is updated on `createUser`
-			// and overwrite the alertId created in the formatter.
-			// This needs to be cleaned up.
-			if (dbUser.exists) {
-				user.alertId = dbUser.data()!.alertId;
-				user.isAdmin = dbUser.data()!.isAdmin;
-				user.apiKey = dbUser.data()!.apiKey;
-				user.medication = dbUser.data()!.medication || "";
-				user.monoclonalAntibody = dbUser.data()!.monoclonalAntibody || "";
-				delete userWithoutToken.alertId;
-			}
+      // TODO(michael) If user already exist then remove alertId
+      // so it isn't overwritten when the user is updated on `createUser`
+      // and overwrite the alertId created in the formatter.
+      // This needs to be cleaned up.
+      if (dbUser.exists) {
+        user.alertId = dbUser.data()!.alertId
+        user.isAdmin = dbUser.data()!.isAdmin
+        user.apiKey = dbUser.data()!.apiKey
+        user.medication = dbUser.data()!.medication || ''
+        user.monoclonalAntibody = dbUser.data()!.monoclonalAntibody || ''
+        delete userWithoutToken.alertId
+      }
 
-			await createUser(user.uid, userWithoutToken);
-			setUser(user);
+      await createUser(user.uid, userWithoutToken)
+      setUser(user)
 
-			cookie.set("hemolog-auth", {
-				expires: 1,
-			});
+      cookie.set('hemolog-auth', {
+        expires: 1,
+      })
 
-			setLoading(false);
-			return user;
-		} else {
-			setUser(null);
-			cookie.remove("hemolog-auth");
+      setLoading(false)
+      return user
+    } else {
+      setUser(null)
+      cookie.remove('hemolog-auth')
 
-			setLoading(false);
-			return false;
-		}
-	};
+      setLoading(false)
+      return false
+    }
+  }
 
-	const auth = firebase.auth();
-	// use emulator if developing locally
-	if (process.env.NEXT_PUBLIC_USE_EMULATORS) {
-		auth.useEmulator("http://localhost:9099");
-	}
+  const auth = firebase.auth()
+  // use emulator if developing locally
+  if (process.env.NEXT_PUBLIC_USE_EMULATORS) {
+    auth.useEmulator('http://localhost:9099')
+  }
 
-	const signinWithGoogle = (redirect: string) => {
-		setLoading(true);
-		return auth
-			.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-			.then((response) => {
-				handleUser(response.user);
+  const signinWithGoogle = (redirect: string) => {
+    setLoading(true)
+    return auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((response) => {
+        handleUser(response.user)
 
-				if (redirect) {
-					Router.push(redirect);
-				}
-			})
-			.catch(() => {
-				setLoading(false);
-			});
-	};
+        if (redirect) {
+          Router.push(redirect)
+        }
+      })
+      .catch(() => {
+        setLoading(false)
+      })
+  }
 
-	const signinWithTestUser = async () => {
-		setLoading(true);
+  const signinWithTestUser = async () => {
+    setLoading(true)
 
-		try {
-			const authResponse = await fetch(
-				`http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signUp?key=testing-key`,
-				{
-					body: JSON.stringify({
-						email: "michael+test@hemolog.com",
-						password: "test123",
-					}),
-					method: "POST",
-					headers: { "Content-Type": "application/json" },
-				},
-			).then((response) => response.json());
+    try {
+      const authResponse = await fetch(
+        `http://localhost:9099/identitytoolkit.googleapis.com/v1/accounts:signUp?key=testing-key`,
+        {
+          body: JSON.stringify({
+            email: 'michael+test@hemolog.com',
+            password: 'test123',
+          }),
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+        }
+      ).then((response) => response.json())
 
-			// stricktly for testing purposes
-			if (authResponse.error?.message === "EMAIL_EXISTS") {
-				return auth
-					.signInWithEmailAndPassword("michael+test@hemolog.com", "test123")
-					.then((response) => {
-						handleUser(response.user);
-						Router.push("/home");
-					})
+      // stricktly for testing purposes
+      if (authResponse.error?.message === 'EMAIL_EXISTS') {
+        return auth
+          .signInWithEmailAndPassword('michael+test@hemolog.com', 'test123')
+          .then((response) => {
+            handleUser(response.user)
+            Router.push('/home')
+          })
 
-					.catch(() => {
-						setLoading(false);
-					});
-			} else {
-				const testUser = {
-					email: authResponse.email,
-					token: authResponse.idToken,
-					uid: authResponse.localId,
-					displayName: "Michael",
-					photoURL: null,
-					providerData: "password",
-				};
+          .catch(() => {
+            setLoading(false)
+          })
+      } else {
+        const testUser = {
+          email: authResponse.email,
+          token: authResponse.idToken,
+          uid: authResponse.localId,
+          displayName: 'Michael',
+          photoURL: null,
+          providerData: 'password',
+        }
 
-				return auth
-					.signInWithEmailAndPassword("michael+test@hemolog.com", "test123")
-					.then(() => {
-						handleUser(testUser);
-						Router.push("/home");
-					});
-			}
-		} catch (e) {
-			console.error(e);
-			setLoading(false);
-		}
-	};
+        return auth
+          .signInWithEmailAndPassword('michael+test@hemolog.com', 'test123')
+          .then(() => {
+            handleUser(testUser)
+            Router.push('/home')
+          })
+      }
+    } catch (e) {
+      console.error(e)
+      setLoading(false)
+    }
+  }
 
-	const signout = () => {
-		Router.push("/signin");
-		return auth.signOut().then(() => handleUser(false));
-	};
+  const signout = () => {
+    Router.push('/signin')
+    return auth.signOut().then(() => handleUser(false))
+  }
 
-	// biome-ignore lint/correctness/useExhaustiveDependencies: handleUser would cause a loop on every render
-	useEffect(() => {
-		const unsubscribe = auth.onIdTokenChanged(handleUser);
-		return () => unsubscribe();
-	}, [auth]);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: handleUser would cause a loop on every render
+  useEffect(() => {
+    const unsubscribe = auth.onIdTokenChanged(handleUser)
+    return () => unsubscribe()
+  }, [auth])
 
-	return {
-		user,
-		loading,
-		// signinWithEmail,
-		// signinWithGitHub,
-		signinWithGoogle,
-		signinWithTestUser,
-		signout,
-	};
+  return {
+    user,
+    loading,
+    // signinWithEmail,
+    // signinWithGitHub,
+    signinWithGoogle,
+    signinWithTestUser,
+    signout,
+  }
 }
 
 const formatUser = async (rawUser: Record<string, any>): Promise<UserType> => {
-	let idTokenResult;
-	if (rawUser.getIdTokenResult) {
-		idTokenResult = await rawUser.getIdTokenResult();
-	}
-	const token = idTokenResult?.token || rawUser.token;
-	const alertId = await generateUniqueString(6);
+  let idTokenResult
+  if (rawUser.getIdTokenResult) {
+    idTokenResult = await rawUser.getIdTokenResult()
+  }
+  const token = idTokenResult?.token || rawUser.token
+  const alertId = await generateUniqueString(6)
 
-	return {
-		alertId,
-		email: rawUser.email,
-		name: rawUser.displayName,
-		photoUrl: rawUser.photoURL,
-		provider: rawUser.providerData?.[0].providerId || "password",
-		token,
-		uid: rawUser.uid,
-	};
-};
+  return {
+    alertId,
+    email: rawUser.email,
+    name: rawUser.displayName,
+    photoUrl: rawUser.photoURL,
+    provider: rawUser.providerData?.[0].providerId || 'password',
+    token,
+    uid: rawUser.uid,
+  }
+}
 
 // NOTE(michael) This takes care of protecting unauthed users
 // from seeing any protected pages. This could be handled better,
 // but this works for now
 export function ProtectRoute({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode
 }): React.ReactElement | null {
-	const { user, loading } = useAuth();
-	if (typeof window === "undefined") return null;
+  const { user, loading } = useAuth()
+  if (typeof window === 'undefined') return null
 
-	console.error("ProtectedRoute", { user, loading });
+  console.error('ProtectedRoute', { user, loading })
 
-	if (loading && !user) {
-		return <LoadingScreen />;
-	}
+  if (loading && !user) {
+    return <LoadingScreen />
+  }
 
-	const { pathname } = window.location;
+  const { pathname } = window.location
 
-	if (!user) {
-		// allow access to signin and emergency pages
-		if (pathname === "/signin" || pathname.includes("emergency")) {
-			return <>{children}</>;
-		}
+  if (!user) {
+    // allow access to signin and emergency pages
+    if (pathname === '/signin' || pathname.includes('emergency')) {
+      return <>{children}</>
+    }
 
-		// Redirect to signin for all other routes when not authenticated
-		if (typeof window !== "undefined") {
-			Router.replace("/signin");
-			return <LoadingScreen />;
-		}
-		return null;
-	}
+    // Redirect to signin for all other routes when not authenticated
+    if (typeof window !== 'undefined') {
+      Router.replace('/signin')
+      return <LoadingScreen />
+    }
+    return null
+  }
 
-	return <>{children}</>;
+  return <>{children}</>
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,72 +1,72 @@
 import Document, {
-	DocumentContext,
-	Html,
-	Head,
-	Main,
-	NextScript,
-} from "next/document";
-import { ServerStyleSheet } from "styled-components";
-import { CssBaseline } from "@geist-ui/react";
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+import { CssBaseline } from '@geist-ui/react'
 
 export default class MyDocument extends Document {
-	static async getInitialProps(ctx: DocumentContext) {
-		const sheet = new ServerStyleSheet();
-		const originalRenderPage = ctx.renderPage;
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App: any) => (props: any) =>
-						sheet.collectStyles(<App {...props} />),
-				});
-			const initialProps = await Document.getInitialProps(ctx);
-			const styles = CssBaseline.flush();
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        })
+      const initialProps = await Document.getInitialProps(ctx)
+      const styles = CssBaseline.flush()
 
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			};
-		} finally {
-			sheet.seal();
-		}
-	}
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
 
-	render() {
-		const googleRichResultsSchema = {
-			"@context": "https://schema.org",
-			"@type": "Organization",
-			url: "https://hemolog.com",
-			logo: "https://hemolog.com/images/hemolog-logo.png",
-		};
+  render() {
+    const googleRichResultsSchema = {
+      '@context': 'https://schema.org',
+      '@type': 'Organization',
+      url: 'https://hemolog.com',
+      logo: 'https://hemolog.com/images/hemolog-logo.png',
+    }
 
-		return (
-			<Html>
-				<Head>
-					<script
-						type="application/ld+json"
-						dangerouslySetInnerHTML={{
-							__html: JSON.stringify(googleRichResultsSchema),
-						}}
-					/>
-					<link
-						href="https://fonts.googleapis.com/css?family=Nanum+Pen+Script&display=swap"
-						rel="stylesheet"
-					/>
-					<link
-						rel="stylesheet"
-						href="https://unpkg.com/react-vis/dist/style.css"
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+    return (
+      <Html>
+        <Head>
+          <script
+            type='application/ld+json'
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify(googleRichResultsSchema),
+            }}
+          />
+          <link
+            href='https://fonts.googleapis.com/css?family=Nanum+Pen+Script&display=swap'
+            rel='stylesheet'
+          />
+          <link
+            rel='stylesheet'
+            href='https://unpkg.com/react-vis/dist/style.css'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
